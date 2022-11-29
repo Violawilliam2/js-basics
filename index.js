ab5926979e707ee6f562602eea4bdda0b295bb40
Violawilliam2 <98275592+Violawilliam2@users.noreply.github.com>
2022-11-29 22:01:17 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+//console.log('yo u ok cuz');
+
+//alert ('ok');
+
+
+
+/*let person = {
+    name: 'viola',
+    age: 27
+};
+
+console.log('the person name is ');*/
+
+function square (number){
+    return number*number;
+}
+//let number = square(8);
+console.log(square(6));
